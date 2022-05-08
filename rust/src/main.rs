361c8f796db1278d6d124892c7c1862b44c4455e
notusknot <asdf@gmail.com>
2022-05-08 15:45:26 -0700

    Somewhat working product

--- rust/src/main.rs
+++ rust/src/main.rs
@@ -1,5 +1,5 @@
 use itertools::Itertools;
-use rusqlite::{params, Connection, Result};
+use rusqlite::{Connection, Result};
 use std::env;
 
 // thank you to Candy Corvid#0137 for helping me with this!
@@ -26,24 +26,6 @@ fn match_bounds(
     matched_list
 }
 
-fn fake_def_nutrients() -> Vec<i32> {
-    let mut nutrient_vec = vec![];
-    for req in 550..600 {
-        nutrient_vec.push(req);
-    }
-
-    // Iterate from middle out
-    fn cycle<T>(slice: &[T], start_pos: usize) -> impl Iterator<Item = &T> {
-        slice.iter().cycle().skip(start_pos).take(slice.len())
-    }
-
-    let nutrient_vec = cycle(&nutrient_vec, nutrient_vec.len() / 3)
-        .copied()
-        .collect::<Vec<_>>();
-
-    nutrient_vec
-}
-
 fn get_args() -> (i32, i32, usize, usize) {
     let args: Vec<String> = env::args().collect();
 
@@ -55,19 +37,37 @@ fn get_args() -> (i32, i32, usize, usize) {
     return (*low, *up, *meals, *days);
 }
 
+//fn fake_def_nutrients() -> Vec<i32> {
+//    let mut nutrient_vec = vec![];
+//    for req in 550..600 {
+//        nutrient_vec.push(req);
+//    }
+//
+//    // Iterate from middle out
+//    fn cycle<T>(slice: &[T], start_pos: usize) -> impl Iterator<Item = &T> {
+//        slice.iter().cycle().skip(start_pos).take(slice.len())
+//    }
+//
+//    let nutrient_vec = cycle(&nutrient_vec, nutrient_vec.len() / 3)
+//        .copied()
+//        .collect::<Vec<_>>();
+//
+//    nutrient_vec
+//}
+
 #[derive(Debug)]
 struct FoodStruct {
     name: String,
     description: String,
     author: String,
-    kcal: f64,
-    fat: f64,
-    saturates: f64,
-    carbs: f64,
-    sugars: f64,
-    fibre: f64,
-    protein: f64,
-    salt: f64,
+    kcal: i32,
+    fat: i32,
+    saturates: i32,
+    carbs: i32,
+    sugars: i32,
+    fibre: i32,
+    protein: i32,
+    salt: i32,
     ingredients: String,
     method: String,
     difficulty: String,
@@ -75,46 +75,45 @@ struct FoodStruct {
     img_url: String,
 }
 
-fn def_nutrients() -> Result<()> {
-    let conn = Connection::open("food.db").unwrap();
+fn nutrient_names_owned(kcal: &[FoodStruct]) -> Vec<i32> {
+    kcal.iter().map(|p| p.kcal.clone()).collect()
+}
+
+fn main() -> Result<()> {
+    let conn = Connection::open("food.db")?;
     let mut nutrient_vec: Vec<FoodStruct> = vec![];
 
     let mut stmt = conn.prepare("SELECT * FROM foodList")?;
-    let person_iter = stmt.query_map([], |row| {
+    let food_iter = stmt.query_map([], |row| {
         Ok(FoodStruct {
             name: row.get(0)?,
-            description: row.get(1)?,
-            author: row.get(2)?,
-            kcal: row.get(3)?,
-            fat: row.get(4)?,
-            saturates: row.get(5)?,
-            carbs: row.get(6)?,
-            sugars: row.get(7)?,
-            fibre: row.get(8)?,
-            protein: row.get(9)?,
-            salt: row.get(10)?,
+            author: row.get(1)?,
+            description: row.get(2)?,
+            kcal: row.get::<_, f64>(3)? as i32,
+            fat: row.get::<_, f64>(4)? as i32,
+            saturates: row.get::<_, f64>(5)? as i32,
+            carbs: row.get::<_, f64>(6)? as i32,
+            sugars: row.get::<_, f64>(7)? as i32,
+            fibre: row.get::<_, f64>(8)? as i32,
+            protein: row.get::<_, f64>(9)? as i32,
+            salt: row.get::<_, f64>(10)? as i32,
             ingredients: row.get(11)?,
             method: row.get(12)?,
             difficulty: row.get(13)?,
-            servings: row.get(14)?,
+            servings: row.get::<_, f64>(14)? as i32,
             img_url: row.get(15)?,
         })
     })?;
 
-    for person in person_iter {
-        //nutrient_vec.push(person.as_ref().unwrap());
-        //println!("{:?}", person.unwrap());
+    for item in food_iter {
+        nutrient_vec.push(item.unwrap());
     }
-    //println!("{:?}", nutrient_vec);
-    Ok(())
-}
 
-fn main() {
-    //let lower_bound = get_args().0;
-    //let upper_bound = get_args().1;
-    //let meal_amnt = get_args().2;
-    //let total_days = get_args().3;
-    //let nutrient_vec = fake_def_nutrients();
-    //println!("{:?}", match_bounds(nutrient_vec, lower_bound, upper_bound, meal_amnt, total_days))
-    println!("{:?}", def_nutrients());
+    let lower_bound = get_args().0;
+    let upper_bound = get_args().1;
+    let meal_amnt = get_args().2;
+    let total_days = get_args().3;
+    println!("{:?}", match_bounds(nutrient_names_owned(&nutrient_vec), lower_bound, upper_bound, meal_amnt, total_days));
+
+    Ok(())
 }
